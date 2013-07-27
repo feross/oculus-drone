987478ac256497d39a46beb1688a2db11f937b30
John Hiesey <jhiesey@cs.stanford.edu>
2013-07-27 15:29:12 -0700

    printing orientation and removed crud from repo

--- oculus/LibOVR/Lib/MacOS/LibOVR_With_Samples.build/Debug/ovr.build/Objects-normal/x86_64/OVR_Atomic.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/abi/Downloads/OculusSDK/Samples/../LibOVR/Src/Kernel/OVR_Atomic.cpp \
-  /Users/abi/Downloads/OculusSDK/Samples/../LibOVR/Src/Kernel/OVR_Atomic.h \
-  /Users/abi/Downloads/OculusSDK/Samples/../LibOVR/Src/Kernel/OVR_Types.h
